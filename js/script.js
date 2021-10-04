201a6ff94b36cdc4aba282d895c374937fa18072
Ankit492000 <67602911+Ankit492000@users.noreply.github.com>
2021-10-04 22:15:33 +0530

    Change 2

--- js/script.js
+++ js/script.js
@@ -9,29 +9,60 @@ function wish()
     document.getElementById('tophead').innerHTML = wishh;
 }
 wish();
-var user = 0;
-function EnterPass()
-{   if(user ==0){
-        let pass = prompt("Enter the Password");
-        if (pass != "0504") {
-            document.getElementsByTagName("body")[0].innerHTML =" InCorrect Password! Try Again After Refreshing";
-            user++;
-        }
+
+function confirmpass()
+{
+    let pass = document.getElementById("passin").value;
+    if (pass == "0504") {
+        window.location.replace("./home.html");
     }
+    else{
+        document.getElementById("passin").value="";
+        document.getElementById("error").innerHTML = "Wrong passowrd!!"
+        document.getElementById("errmsg").style.display="block";
+    }
+
 }
-// EnterPass();
+function act(ID){
+    var  mnu = document.getElementsByClassName("lst");
+    for(var i=0;i<mnu.length;i++){
+        mnu[i].classList.remove("active");
+    }
+    document.getElementById(ID).classList.add("active");
+}
 
 
 var index =0;
-var slide1imgs = ['./img/slides1/slideimg1.jpg','./img/slides1/slideimg2.jpg','./img/slides1/slideimg3.jpg','./img/slides1/slideimg4.jpg'];
+var slideln=9;
 function nextslide(n)
 {
     slideshow(index += n);
 }
 function slideshow(n){
-    if(n>slide1imgs.length-1){n = 0}
-    if(n<0){n = slide1imgs.length-1}
-    document.getElementById("slide1").src = slide1imgs[n];
+    if(n>slideln){n = 1}
+    if(n<1){n = slideln}
+    document.getElementById("slide1").src = '/img/slides1/slideimg' + n + '.jpg' ;
     index=n;
 }
 setInterval(nextslide,3000,1);
+
+var index2 =0;
+var stopcnt =0
+var slide2ln = 12;
+function nextslide2(n,stop){
+    if(stopcnt%2==0)
+    slideshow2(index2 += n);
+}
+function slideshow2(n){
+    
+    if(n>slide2ln){n = 1}
+    if(n<1){n = slide2ln}
+    document.getElementById("slides2").src = '/img/16-11-2020/' + n + '.jpg';
+    index2=n;
+}
+function stp()
+{
+    stopcnt ++;
+}
+nextslide2(1);
+setInterval(nextslide2,1000,1,stopcnt);
